9326bbdf50be394978aaddd23e4501ce9bc315aa
Islam K. Matar <31720098+IslamMatar@users.noreply.github.com>
2024-07-03 12:57:40 -0300

    Update c_ring_aln_fun.py

--- c_ring_aln_fun.py
+++ c_ring_aln_fun.py
@@ -2,6 +2,18 @@ import csv
 from pymol import cmd
 from pymol import stored
 
+def construct_bioassembly(pdb_list=pdb_list, bio_assembly=bio_assembly):
+    for pdb in pdb_list:
+        cmd.fetch(pdb, type='cif', path='./1_input_pdb')
+        cmd.split_states(pdb)
+        cmd.delete(pdb)
+        assembly_parts = cmd.get_names(pdb+'*')
+        for i in range(1, len(assembly_parts)+1):
+            cmd.alter(assembly_parts[i-1], f'chain=chain+str({i})')
+        cmd.create(pdb, assembly_parts)
+        cmd.delete(f'not {pdb}')
+        cmd.save(f'{pdb}.cif', format='cif')
+
 def c_ring_pseudoatoms(pdb):
     print('Processing PDB ID: '+pdb[0])
     cmd.select(pdb[0].lower()+'_c_ring', '('+'chain '+pdb[1].replace(',','+')+') and '+pdb[0])
@@ -21,6 +33,7 @@ def c_ring_cleanup(pdb):
     cmd.delete(pdb[0].lower()+'_c_ring_cen_axs')
 
 
+cmd.extend("construct_bioassembly", construct_bioassembly)
 cmd.extend("c_ring_pseudoatoms", c_ring_pseudoatoms)
 cmd.extend("c_ring_align", c_ring_align)
 cmd.extend("c_ring_cleanup", c_ring_cleanup)
